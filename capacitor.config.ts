2ec92dba2301e7b4fb4cc804a12661d6fefca864
kurt112 <kurtorioque112@gmail.com>
2024-08-04 03:20:02 +0800

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'blood-donor-receiver',
+  webDir: 'www'
+};
+
+export default config;
